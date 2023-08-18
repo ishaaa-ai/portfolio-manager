e09c936e5c91ec9641c595293bfc91b54a640c70
ishaaa-ai <isha.kerpal@gmail.com>
2023-08-18 19:03:14 +0000

    Add methods to Portfolio repository

--- spring-project/src/main/java/com/springproject/springproject/repos/PortfolioRepository.java
+++ spring-project/src/main/java/com/springproject/springproject/repos/PortfolioRepository.java
@@ -1,10 +1,14 @@
 package com.springproject.springproject.repos;
 
-import com.springproject.springproject.entities.Portfolio;
+import com.springproject.springproject.entities.PortfolioStock;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Collection;
 
-public interface PortfolioRepository extends JpaRepository<Portfolio, Integer> {
-    Collection<Portfolio> findByStockId(Integer stockId);
+public interface PortfolioRepository extends JpaRepository<PortfolioStock, Integer> {
+    Collection<PortfolioStock> findByStockId(Integer stockId);
+
+    void addByStockId(Integer stockId);
+
+    void deleteByStockId(Integer stockId);
 }
