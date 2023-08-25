f23bec9623284d6ff8d05eb019d221f992b6563f
elicir <ericajjf.li@gmail.com>
2023-08-25 20:55:17 +0000

    added endpoint for change from previous to current day

--- backend/spring-project/src/main/java/com/springproject/springproject/service/PriceService.java
+++ backend/spring-project/src/main/java/com/springproject/springproject/service/PriceService.java
@@ -3,15 +3,18 @@ package com.springproject.springproject.service;
 import com.springproject.springproject.entities.Price;
 import org.springframework.data.jpa.repository.Query;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 public interface PriceService {
     List<Price> getPrices();
     List<Price> getPriceByStock(Integer id);
     List<Price> getPriceByTicker(String symbol);
-    List<Price> getPriceByTickerAndDate(String symbol, String startDate, String endDate);
+    List<Price> getPriceByTickerAndDate(String symbol, LocalDateTime startDate, LocalDateTime endDate);
     List<Price> getPriceByDate(String startDate, String endDate);
 
     Double getPercentChange(String symbol, String startDate, String endDate);
+
+    Double getPercentChangeToday(String symbol, String todayDate);
 
 }
